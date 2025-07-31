f0d740e4780e4bddd5cadef71df8c506145428cf
adriel morais <adrie3lmorais@gmail.com>
2025-07-31 20:07:46 -0300

    Configurando a URL do backend no axiosConfig

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,42 +1,27 @@
 import axios from 'axios';
 
-const api = axios.create({
- baseURL: 'http://localhost:8081', // <--- Mude para isto!
+// A URL base do seu backend no Render
+const backendUrl = 'https://seusanimeslist-backendd2.onrender.com';
+
+const axioConfig = axios.create({
+    baseURL: backendUrl,
     headers: {
-        'Content-Type': 'application/json',
-    },
+        'Content-Type': 'application/json'
+    }
 });
 
-// Interceptor para adicionar o token JWT a cada lll
-api.interceptors.request.use(
-    (config) => {
+// Adicionando um interceptor para o token JWT
+axioConfig.interceptors.request.use(
+    config => {
         const token = localStorage.getItem('jwtToken');
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
-            console.log("Axios Interceptor: Adicionando cabeçalho Authorization com token.");
-        } else {
-            console.warn("Axios Interceptor: Token JWT não encontrado no localStorage.");
         }
         return config;
     },
-    (error) => {
+    error => {
         return Promise.reject(error);
     }
 );
 
-// Opcional: Interceptor para lidar com 401 e limpar o token
-api.interceptors.response.use(
-    (response) => response,
-    (error) => {
-        if (error.response && error.response.status === 401) {
-            console.error("Axios Interceptor: 401 Unauthorized. Limpando token.");
-            localStorage.removeItem('jwtToken');
-            // Opcional: Você pode redirecionar o usuário para a página de login aqui
-            // window.location.href = '/login'; 
-            // so pra subir
-        }
-        return Promise.reject(error);
-    }
-);
-
-export default api;
+export default axioConfig;
